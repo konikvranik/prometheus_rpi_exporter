2af1188d216c30827ab53ccd2b0f586f9dcae080
Petr Vranik <petr@vranik.name>
2018-10-31 11:35:12 +0100

    kodi addon

---
+++ kodi_addon/script.service.rpi_exporter/service.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, StateSetMetricFamily, REGISTRY
+import time
+import socket
+
+class RPiCollector(object):
+
+    def collect(self):
+        f = open('/sys/class/thermal/thermal_zone0/temp', 'r')
+        temperature = int(f.read()) * 0.001
+        metric = GaugeMetricFamily('temperature', 'RPi temperature', labels=['node', 'domain','type', 'sensor', 'unit'])
+        metric.add_metric([str(socket.gethostname()), 'sensor', 'temperature', 'rpi', u'°C'], temperature)
+        yield metric
+
+if __name__ == "__main__":
+    REGISTRY.register(RPiCollector())
+    start_http_server(9119)
+    while True:
+        time.sleep(1)
